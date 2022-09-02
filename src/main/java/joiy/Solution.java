82c55724fd3302659b736b623daf30b20d56cc51
joiy908 <joiy9088@gmail.com>
2022-09-02 12:03:16 +0800

    L2 third-version: just do add myself

--- src/main/java/joiy/Solution.java
+++ src/main/java/joiy/Solution.java
@@ -32,14 +32,52 @@ public class Solution {
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // 1 get num1, num2 : use bigInteger
-        // 2 get sum
-        // 3 generate ListNode and return
+        // way2: just calculate
+        ListNode ptr1 = l1;
+        ListNode ptr2 = l2;
+        ListNode rst = new ListNode(); // uninitialized
+        ListNode curr = rst;
+        // 进位值
+        int addedNum = 0;
+        int thisDigit;
+        while (ptr1 != null || ptr2 != null) {
+            int sum;
+            // get sum and update ptr1 and ptr2
+            if (ptr1 == null) {
+                sum = ptr2.val + addedNum;
+                ptr2 = ptr2.next;
+            }else if (ptr2 == null) {
+                sum = ptr1.val + addedNum;
+                ptr1 = ptr1.next;
+            }else{// else both != null
+                sum = ptr1.val + ptr2.val + addedNum;
+                ptr1 = ptr1.next;
+                ptr2 = ptr2.next;
+            }
 
-        BigInteger n1 = getBigInter(l1);
-        BigInteger n2 = getBigInter(l2);
-        BigInteger sum = n1.add(n2);
-        return makeListNode(sum);
+            // update thisDigit and addedNum
+            if (sum > 9) {
+                thisDigit = sum - 10;
+                addedNum = 1;
+            }else {
+                thisDigit = sum;
+                addedNum = 0;
+            }
+
+            // put thisDigit to curr and update curr
+            // case1: extreme case
+            if (ptr1 == null && ptr2 == null) {
+                curr.val = thisDigit;
+                if (addedNum > 0) {
+                    curr.next = new ListNode(addedNum);
+                }
+                return rst;
+            }// case2: normal
+            curr.val = thisDigit;
+            curr.next = new ListNode();
+            curr = curr.next;
+        }
+        return rst;
     }
 
     private ListNode makeListNode(BigInteger sum) {
@@ -98,10 +136,10 @@ public class Solution {
     @Test
     public void testGetLen() {
         // --- test getLen()
-        ListNode node1 = new ListNode(1);
+        ListNode node1 = new ListNode(2);
         Assertions.assertEquals(getLen(node1), 1);
 
-        ListNode node2 = new ListNode(2);
+        ListNode node2 = new ListNode(4);
         node1.next = node2;
         Assertions.assertEquals(getLen(node1), 2);
 
@@ -111,22 +149,26 @@ public class Solution {
 
         // --- test getBigInteger()
         BigInteger b1 = getBigInter(node1);
-        Assertions.assertEquals(b1, new BigInteger("321"));
+//        Assertions.assertEquals(b1, new BigInteger("321"));
 
         ListNode node0 = new ListNode(0);
         node0.next = node1;
-        Assertions.assertEquals(getBigInter(node0), new BigInteger("3210"));
+//        Assertions.assertEquals(getBigInter(node0), new BigInteger("3210"));
 
         node3.next = new ListNode(0);
-        Assertions.assertEquals(getBigInter(node1), new BigInteger("0321"));
+//        Assertions.assertEquals(getBigInter(node1), new BigInteger("0321"));
 
 
         BigInteger sum = b1.add(new BigInteger("1"));
 //        System.out.println("sum = " + sum);
 
-        System.out.println(getBigInter(node1));
-        System.out.println(getBigInter(node2));
-        System.out.println(addTwoNumbers(node1, node2));
+//        System.out.println(getBigInter(node1));
+//        System.out.println(getBigInter(node2));
+        ListNode b2 = makeListNode(new BigInteger("342"));
+        ListNode b3 = makeListNode(new BigInteger("465"));
+
+
+        System.out.println(addTwoNumbers(b2, b3));
     }
 }
 
