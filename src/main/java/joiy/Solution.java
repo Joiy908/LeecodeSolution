18d07f1bf08c39347e3c428af1bea1f5ec997e9f
joiy908 <joiy9088@gmail.com>
2022-09-18 16:49:12 +0800

    L7 solution

--- src/main/java/joiy/Solution.java
+++ src/main/java/joiy/Solution.java
@@ -7,37 +7,40 @@ import java.util.LinkedList;
 
 public class Solution {
     /**
-     * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列
-     *
+     * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+     * 如果反转后整数超过 32 位的有符号整数的范围 ，就返回 0。
      */
-    public String convert(String s, int numRows) {
-        if (numRows == 1) return s;
-        // new n list
-        // use a flag to decide curr item to which list
+    public int reverse(int x) {
+        long n = 0;
+        while(x != 0) {
+            n = n*10 + x%10;
 
-        LinkedList<StringBuilder> bin = new LinkedList<>();
-        for (int i = 0; i < numRows; i++) {
-            bin.add(new StringBuilder());
+            x = x/10;
         }
-        int flag = -1, target = 1;
-        for (char c : s.toCharArray()) {
-            target += flag;
-            bin.get(target).append(c);
-            if (target == 0 || target == numRows - 1) flag = -flag;
-        }
-        StringBuilder rst = new StringBuilder(s.length());
-        for (StringBuilder sb : bin) {
-            rst.append(sb);
-        }
-        return rst.toString();
+        return (int)n==n? (int)n:0;
     }
 
     @Test
     public void main() {
-        String s = "PAYPALISHIRING";
-        int numRows = 3;
-        final String rst = convert(s, numRows);
-        Assertions.assertEquals(rst, "PAHNAPLSIIGYIR");
+
+        int x = 123, ex = 321;
+        Assertions.assertEquals(ex, reverse(x));
+
+        x = -123;
+        ex = -321;
+        Assertions.assertEquals(ex, reverse(x));
+
+        x = 120;
+        ex = 21;
+        Assertions.assertEquals(ex, reverse(x));
+
+        x = 0;
+        ex = 0;
+        Assertions.assertEquals(ex, reverse(x));
+
+
+
+
     }
 
 }
