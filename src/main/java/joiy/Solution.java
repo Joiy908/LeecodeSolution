f143963ac267fa6bf939499c8ffd18591d2b4400
joiy908 <joiy9088@gmail.com>
2022-09-20 19:13:32 +0800

    L8 unfinished shit-like code

--- src/main/java/joiy/Solution.java
+++ src/main/java/joiy/Solution.java
@@ -3,40 +3,95 @@ package joiy;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.LinkedList;
-
 public class Solution {
     /**
-     * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
-     * 如果反转后整数超过 32 位的有符号整数的范围 ，就返回 0。
+     * 请你来实现一个 myAtoi(string s) 函数，使其能将字符串转换成一个 32 位有符号整数
      */
-    public int reverse(int x) {
-        long n = 0;
-        while(x != 0) {
-            n = n*10 + x%10;
-
-            x = x/10;
+    public int myAtoi(String s) {
+        if (s.equals("")) return 0;
+        // 1. trim: find start and end
+        int start = 0, end = 0, sign;
+        // 1.1 find start
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if (c == ' ') start++;
+            else if(isDigit(c) || isSign(c)) break;
+            else return 0;
         }
-        return (int)n==n? (int)n:0;
+        if (start > s.length() -1) return 0;
+        if (isSign(chars[start])) {
+            sign = chars[start] == '-' ? -1 : 1;
+            start++;
+        }else sign = 1;
+        // rm all 0 before other
+        for (int i = start; i < s.length(); i++) {
+            if (chars[i] == '0') start++;
+            else break;
+        }
+        // 1.2 find end
+        end = start;
+        while(end < s.length()) {
+            if (!isDigit(chars[end])) break;
+            end++;
+        }
+        end--;
+
+        int rst = 0, last, lastJ;
+        // 2. reversely get rst
+        for (int i = end, j = 1; i >= start ; i--) {
+            last = rst;
+            lastJ = j;
+            rst += (chars[i] - '0') * j;
+            j *= 10;
+            if (last != rst%lastJ || j/10 != lastJ) { // overflow
+                return -1 == sign ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+        }
+        return sign * rst;
+    }
+
+    public boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+    public boolean isSign(char c) {
+        return c == '-' || c == '+';
     }
 
     @Test
     public void main() {
 
-        int x = 123, ex = 321;
-        Assertions.assertEquals(ex, reverse(x));
+        String s;
+        int ex;
+        s = "42";
+        ex = 42;
+        Assertions.assertEquals(ex, myAtoi(s));
 
-        x = -123;
-        ex = -321;
-        Assertions.assertEquals(ex, reverse(x));
+        s = "    -42";
+        ex = -42;
+        Assertions.assertEquals(ex, myAtoi(s));
 
-        x = 120;
+        s = "21 with word";
         ex = 21;
-        Assertions.assertEquals(ex, reverse(x));
+        Assertions.assertEquals(ex, myAtoi(s));
 
-        x = 0;
+        s = "sdf21 with word";
         ex = 0;
-        Assertions.assertEquals(ex, reverse(x));
+        Assertions.assertEquals(ex, myAtoi(s));
+
+        s = "";
+        Assertions.assertEquals(ex, myAtoi(s));
+
+        s = "+1";
+        ex = 1;
+        Assertions.assertEquals(ex, myAtoi(s));
+
+        s = "-000000000000000000000000000000000000000000000000001";
+        ex = -1;
+        Assertions.assertEquals(ex, myAtoi(s));
+
+        s = "    10522545459";
+        ex = Integer.MAX_VALUE;
+        Assertions.assertEquals(ex, myAtoi(s));
 
 
 
