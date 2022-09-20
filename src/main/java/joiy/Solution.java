40a9cfa09b5f87858bc627f3f4e6aa93e97ee817
joiy908 <joiy9088@gmail.com>
2022-09-20 21:21:28 +0800

    L7 modify

--- src/main/java/joiy/Solution.java
+++ src/main/java/joiy/Solution.java
@@ -9,15 +9,19 @@ public class Solution {
     /**
      * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
      * 如果反转后整数超过 32 位的有符号整数的范围 ，就返回 0。
+     * deal with overflow: way2
      */
     public int reverse(int x) {
-        long n = 0;
+        int n = 0;
         while(x != 0) {
+            if (x > 0 && n > (Integer.MAX_VALUE - x%10)/10)
+                return 0;
+            if (x < 0 && n < (Integer.MIN_VALUE - x%10)/10)
+                return 0;
             n = n*10 + x%10;
-
             x = x/10;
         }
-        return (int)n==n? (int)n:0;
+        return n;
     }
 
     @Test
